6fa6b44066065494d186fdb928931d8a2e06bd36
Alok23-dev <alokppandey23@gmail.com>
2026-02-03 21:13:47 +0530

    Add files via upload
    flickker

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-let bulb = document.querySelector("#bulb");
 let button = document.querySelector("button");
-    
-let flag =0;
+let main = document.querySelector("main");
 
-button.addEventListener("click", function() {
-    if(flag==0){ bulb.style.backgroundColor = "yellow";
+
+button.addEventListener("click",function(){
+    let div= document.createElement("div");
+    div.style.width="200px";
+    div.style.height="100px";
+    div.style.backgroundColor="red";
+
+    let x= Math.random()*100;
+     let y= Math.random()*100;
+     let r = Math.floor(Math.random()*256);
+     let g = Math.floor(Math.random()*256);
+     let b =Math.floor(Math.random()*256);
+
+     div.style.position="absolute";
+     div.style.top= y+"%";
+     div.style.left= x +"%";
+     div.style.backgroundColor= `rgb( ${r}, ${g},${b})`;
     
-    button.innerText = "Off";
-    flag=1;}
-    else{ bulb.style.backgroundColor = "rgb(248, 248, 248)";
-   flag=0;}
-    button.innerText = flag==0?"On":"Off";
     
-})
+
+    main.appendChild(div);
+
+});
